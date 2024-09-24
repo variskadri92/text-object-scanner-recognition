696116c78cf410884ca99821f717d2bb51f64df9
yashg92 <ygworld76@gmail.com>
2024-09-24 17:47:43 +0530

    test

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:text_and_object_scanner/recognition_screen.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(Homescreen());
@@ -17,10 +18,9 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-
-
   File? _image;
   String? _ocrText;
+  bool _isLoading = false;
 
   void _navigateToRecognitionScreen() {
     if (_image != null && _ocrText != null) {
@@ -35,10 +35,14 @@ class _HomescreenState extends State<Homescreen> {
     }
   }
 
-
   Future<void> _sendImageToServer(File image) async {
-    final request = http.MultipartRequest('POST', Uri.parse('http://192.168.92.108:5000/api'));
+    setState(() {
+      _isLoading = true;
+    });
+
+    final request = http.MultipartRequest('POST', Uri.parse('http://192.168.43.5:5000/api'));
     request.files.add(await http.MultipartFile.fromPath('image', image.path));
+
     final response = await request.send();
 
     if (response.statusCode == 200) {
@@ -46,15 +50,19 @@ class _HomescreenState extends State<Homescreen> {
       final decoded = json.decode(responseBody);
       setState(() {
         _ocrText = decoded['text'];
+        _isLoading = false;
       });
       print(_ocrText);
+      _navigateToRecognitionScreen(); // Navigate to the recognition screen after OCR text is set
     } else {
+      setState(() {
+        _isLoading = false;
+      });
       print('Error: ${response.reasonPhrase}');
     }
   }
 
   Future<void> _pickImage() async {
-    print('hwlo');
     final picker = ImagePicker();
     try {
       final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -62,6 +70,7 @@ class _HomescreenState extends State<Homescreen> {
         setState(() {
           _image = File(pickedFile.path);
         });
+        print('Sending to server');
         await _sendImageToServer(_image!);
         print(_image.toString());
       } else {
@@ -71,69 +80,126 @@ class _HomescreenState extends State<Homescreen> {
       print('Error picking image: $e');
     }
   }
-  @override
-  void initState(){
-    super.initState();
-
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding:EdgeInsets.only(top:30),
-            child: Card(
-              color: Colors.blueAccent,
-              child: Container(
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return MaterialApp(
+      home: Scaffold(
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Container(
+                color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Column(
-                      children: [
-                        IconButton(icon: Icon(Icons.scanner,size: 50,color: Colors.white,),onPressed: (){},),
-                        Text("Scan",style: TextStyle(color: Colors.white),),
-                      ],
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: Card(
+                        color: Colors.blueAccent,
+                        child: Container(
+                          height: 100,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.scanner,
+                                      size: 50,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                  Text(
+                                    "Scan",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.assignment_sharp,
+                                      size: 50,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: () {
+                                      _navigateToRecognitionScreen();
+                                    },
+                                  ),
+                                  Text(
+                                    "Recognize",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.image,
+                                      size: 50,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                  Text(
+                                    "Enhance",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    Column(
-                      children: [
-                        IconButton(icon: Icon(Icons.assignment_sharp,size: 50,color: Colors.white,),onPressed: (){_navigateToRecognitionScreen();},),
-                        Text("Recognize",style: TextStyle(color: Colors.white),),
-                      ],
+                    Card(
+                      child: Container(
+                        height: MediaQuery.of(context).size.height - 300,
+                        child: _image == null ? Text('No image selected') : Image.file(_image!),
+                      ),
                     ),
-                    Column(
-                      children: [
-                        IconButton(icon: Icon(Icons.image,size: 50,color: Colors.white,),onPressed: (){},),
-                        Text("Enhance",style: TextStyle(color: Colors.white),),
-                      ],
+                    Card(
+                      color: Colors.blueAccent,
+                      child: Container(
+                        height: 100,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                Icons.refresh_rounded,
+                                size: 50,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                Icons.camera,
+                                size: 50,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                Icons.image,
+                                size: 50,
+                                color: Colors.white,
+                              ),
+                              onPressed: () => _pickImage(),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ),
-          Card(child: Container(height: MediaQuery.of(context).size.height-300,
-            child: _image ==null? Text('no image') : Image.file(_image!), 
-          ),),
-          Card(
-            color: Colors.blueAccent,
-            child: Container(
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(icon: Icon(Icons.refresh_rounded,size: 50,color: Colors.white,),onPressed: (){},),
-                  IconButton(icon: Icon(Icons.camera,size: 50,color: Colors.white,),onPressed: (){},),
-                  IconButton(icon: Icon(Icons.image,size: 50,color: Colors.white,),onPressed:()=> _pickImage()),
-                ],
-              ),
-            ),
-          )
-        ],
       ),
     );
   }
